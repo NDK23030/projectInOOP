8525f16ba295df6ca3877416d405ec4301932e40
Denis Mazanov <ggmazanovdenis@gmail.com>
2023-06-03 02:38:13 +0300

    create mini sliders

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,37 @@
 import SliderMain from "./modules/slider/sliderMain";
+import SliderMini from "./modules/slider/sliderMini";
 import VideoPlayer from "./modules/playVideo";
 
 window.addEventListener('DOMContentLoaded', () => {
-    const slider = new SliderMain({btns: '.next', page: '.page'});
+    const slider = new SliderMain({btns: '.next', container: '.page'});
     slider.render();
+
+    const showUpSlider = new SliderMini({
+        container: '.showup__content-slider', 
+        prev: '.showup__prev', 
+        next: '.showup__next',
+        activeClass: 'card-active',
+        animate: true
+    });
+    showUpSlider.init();
+
+    const modulesSlider = new SliderMini({
+        container: '.modules__content-slider', 
+        prev: '.modules__info-btns .slick-prev', 
+        next: '.modules__info-btns .slick-next',
+        activeClass: 'card-active',
+        animate: true,
+        autoplay: true
+    });
+    modulesSlider.init();
+
+    const feedSlider = new SliderMini({
+        container: '.feed__slider', 
+        prev: '.feed__slider .slick-prev', 
+        next: '.feed__slider .slick-next',
+        activeClass: 'feed__item-active'
+    });
+    feedSlider.init(); 
 
     const player = new VideoPlayer('.showup .play', '.overlay');
     player.init();
