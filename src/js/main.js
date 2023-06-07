59585e2b31fb559e73fa62d12d45b754688cfd3d
Denis Mazanov <ggmazanovdenis@gmail.com>
2023-06-08 00:45:43 +0300

    module slider

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,9 @@ window.addEventListener('DOMContentLoaded', () => {
     const slider = new SliderMain({btns: '.next', container: '.page'});
     slider.render();
 
+    const modulePageSlider = new SliderMain({container: '.moduleapp', btns: '.next'});
+    modulePageSlider.render();
+
     const showUpSlider = new SliderMini({
         container: '.showup__content-slider', 
         prev: '.showup__prev', 
